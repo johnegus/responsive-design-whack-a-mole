e5942c1b0028eaf58d80860fae71da7ef2a33451
john <jgh2102@gmail.com>
2020-09-12 02:20:14 +0900

    trying to iterate through moleheads

--- mole.js
+++ mole.js
@@ -1,9 +1,21 @@
 // Write your JS here.
 window.addEventListener('DOMContentLoaded', () => {
-    setInterval(() => {
-        const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    // setInterval(() => {
+    //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+        // for (let moleHead of moleHeads) {
+        //     moleHead.classList.toggle('wgs__mole-head--hidden');
+        // }
+    // },1000)
+
+    function popUpRandomMole(){
+        let moleHeads = document.querySelectorAll('.wgs__mole-head');
+        let randomNumber = Math.floor(Math.random()*8)
         for (let moleHead of moleHeads) {
-            moleHead.classList.toggle('wgs__mole-head--hidden');
+            console.log(moleHead);
+            moleHead[randomNumber].classList.remove('wgs__mole-head--hidden');
         }
-    },1000)
+        // randomMole.remove.classList('wgs__mole-head--hidden');
+    }
+    popUpRandomMole();
 });
+
