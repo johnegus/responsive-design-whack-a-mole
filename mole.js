530e79bc00917a5a59ac4043d2542a29a67304d8
Robin Scavo <sirscavo@gmail.com>
2020-09-11 13:44:53 -0600

    DONE

--- mole.js
+++ mole.js
@@ -3,12 +3,13 @@
 
     function popUpRandomMole(){
         // let moleHeads = document.querySelectorAll('.wgs__mole-head');
-        const moleHeads = document.querySelectorAll('.wgs__mole-head');
+        const moleHeads = document.querySelectorAll('.wgs__mole-head:not(.wgs__mole-head--whacked');
+        if (moleHeads.length === 0) return alert ("YOU WIN!!!")
         let randomNumber = Math.floor(Math.random()* moleHeads.length);
         let moleHead = moleHeads[randomNumber];
         // console.log(moleHead)
         moleHead.classList.remove("wgs__mole-head--hidden");
-        setTimeout(()=>hideMole(moleHead), 1000)
+        setTimeout(()=>hideMole(moleHead), 3000)
     }
     function hideMole (moleHead) {
         // let randomNumber = Math.floor(Math.random()*8)
@@ -19,4 +20,12 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     setTimeout(popUpRandomMole, 0);
+    let moleHeads = document.querySelectorAll('.wgs__mole-head');
+    for (let moleHead of moleHeads) {
+        moleHead.addEventListener('click', event => {
+            event.target.classList.add('wgs__mole-head--hidden');
+            event.target.classList.add('wgs__mole-head--whacked');
+
+        })
+    }
 });
