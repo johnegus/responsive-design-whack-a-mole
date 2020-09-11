68aa2c4d30c6d90c598a12904dad7b606f5519c5
john <jgh2102@gmail.com>
2020-09-12 03:14:53 +0900

    moles pop up and hide

--- mole.js
+++ mole.js
@@ -1,25 +1,22 @@
 // Write your JS here.
-window.addEventListener('DOMContentLoaded', () => {
-    // setInterval(() => {
-    //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
-        // for (let moleHead of moleHeads) {
-        //     moleHead.classList.toggle('wgs__mole-head--hidden');
-        // }
-    // },1000)
+
 
     function popUpRandomMole(){
         // let moleHeads = document.querySelectorAll('.wgs__mole-head');
-        let randomNumber = Math.floor(Math.random()*8)
-        let moleHead = document.getElementById(`${randomNumber}`);
-        console.log(moleHead)
+        const moleHeads = document.querySelectorAll('.wgs__mole-head');
+        let randomNumber = Math.floor(Math.random()* moleHeads.length);
+        let moleHead = moleHeads[randomNumber];
+        // console.log(moleHead)
         moleHead.classList.remove("wgs__mole-head--hidden");
-        setTimeout(hideMole(moleHead), 1000)
+        setTimeout(()=>hideMole(moleHead), 1000)
     }
     function hideMole (moleHead) {
         // let randomNumber = Math.floor(Math.random()*8)
         // let moleHead = document.getElementById(`${randomNumber}`);
         moleHead.classList.add("wgs__mole-head--hidden");
-        setTimeout(popUpRandomMole(), 1000)
+        setTimeout(popUpRandomMole, 1000)
     }
-    setTimeout(popUpRandomMole(), 0);
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(popUpRandomMole, 0);
 });
